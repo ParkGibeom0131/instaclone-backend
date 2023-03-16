52b240849fc1635168a322cb4b76ec7589ca25a7
Yeonlii <ptj0131@gmail.com>
2023-03-17 01:53:43 +0900

    complete login sector

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,5 +1,6 @@
 import client from './../client';
 import bcrypt from "bcrypt";
+import Jwt from 'jsonwebtoken';
 
 export default {
     Mutation: {
@@ -44,6 +45,33 @@ export default {
             }
 
             // save and return the user 
+        },
+
+        login: async (_, { username, password }) => {
+            // find user with args.username
+            const user = await client.user.findFirst({ where: { username } });
+            if (!user) {
+                return {
+                    ok: false,
+                    error: "User not found.",
+                };
+            }
+
+            // check password with args.password
+            const passwordOk = await bcrypt.compare(password, user.password);
+            if (!passwordOk) {
+                return {
+                    ok: false,
+                    error: "Incorrect password.",
+                };
+            }
+
+            // issue a token and send it to the user
+            const token = await Jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+            return {
+                ok: true,
+                token,
+            };
         },
     },
 };
